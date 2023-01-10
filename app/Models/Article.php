7f00aebdd7b109779df382491c77b51bf4669a99
Sezam <dossumbekov@list.ru>
2023-01-11 05:14:57 +0600

    add: View main page

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Str;
 
 class Article extends Model
 {
@@ -19,6 +20,15 @@ class Article extends Model
 
     public function tags() {
         return $this->belongsToMany(Tag::class);
+    }
+
+    public function getBodyPreview(){
+        return Str::limit($this->body, 100);
+    }
+
+    public function createdAtForHumans(){
+        return $this->created_at->diffForHumans();
+//        return $this->published_at->diffForHumans();
     }
 
     use HasFactory;
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
+
 const mix = require('laravel-mix');
 
 /*
@@ -13,4 +14,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
+
+mix.copy('resources/img', 'public/img');
